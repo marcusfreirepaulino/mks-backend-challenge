31c92a7f8f4a0ab97a7461cf3d4c1cfbaa32b5f7
Marcus Vinicius <marcusfreirepaulino@gmail.com>
2024-04-12 15:46:55 -0300

    adjust folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DBConnection } from './data/config/database.config';
 
 async function bootstrap() {
+  await DBConnection.initialize();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
